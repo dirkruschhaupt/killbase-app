f3f2118bd82df473ca5f55184cf4c7f2324de06f
Dirk Ruschhaupt <dirkruschhaupt@Dirks-MacBook-Pro.local>
2018-07-02 15:34:01 -0500

    make it better

--- router/assassinsRouter.js
+++ router/assassinsRouter.js
@@ -1,21 +1,49 @@
 let router = require('express').Router();
 
 router.get('/assassins', function(req,res) {
-  let responceText = 'A Get request for the assassins';
-  console.log(responceText);
-  res.status(200).send(responceText);
+  knex('assassins')
+    .orderBy('id')
+    .then((assassins) => {
+      res.send(assassins);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.get('/assassins/:id', function(req, res) {
-  let responceText = 'A Get request for the assassins/id';
-  console.log(responceText);
-  res.status(200).send(responceText);
+  knex('assassins')
+    .where('id', req.params.id)
+    .first()
+    .then((assassins) => {
+      if (!assassins) {
+        return next();
+      }
+
+      res.send(assassins);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 router.post('/assassins', function(req,res) {
-  let responceText = 'A Post request for the assassins';
-  console.log(responceText);
-  res.status(200).send(responceText);
+  knex('assassins')
+        .insert({
+            full_name: req.body.full_name,
+            weapon: req.body.weapon,
+            contact_info: req.body.contact_info,
+            age: req.body.age,
+            price: req.body.price,
+            rating: req.body.rating,
+            kills: req.body.kills
+        }, '*')
+        .then((assassins) => {
+            res.send(assassins[0]);
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 router.post('/assassins/:id', function(req,res) {
